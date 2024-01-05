3f911b6e7456da2acdefad947adb089359888c8a
Bruno Barbosa <babosa227@gmail.com>
2024-01-05 15:51:42 -0300

    first commit

---
+++ src/test/java/com/books/producerbooks/ProducerBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books.producerbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
